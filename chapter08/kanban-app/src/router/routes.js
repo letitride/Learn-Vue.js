22939248d0d63928c11c6680db9e2f8cda9f0829
letitride <ichikawa.fumiya@gmail.com>
2020-04-20 22:58:21 +0900

    chapter10 route/以下のjsファイルのテスト除外

--- chapter08/kanban-app/src/router/routes.js
+++ chapter08/kanban-app/src/router/routes.js
@@ -1,6 +1,6 @@
 import KbnBoardView from '@/components/templates/KbnBoardView.vue'
 import KbnLoginView from '@/components/templates/KbnLoginView.vue'
-import KbnTaskDetailModel from '@/components/templates/KbnTaskDetailModel.vue'
+import KbnTaskDetailModal from '@/components/templates/KbnTaskDetailModal.vue'
 
 export default [{
   path: '/',
@@ -11,7 +11,7 @@ export default [{
   component: KbnLoginView
 }, {
   path: '/tasks/:id',
-  component: KbnTaskDetailModel,
+  component: KbnTaskDetailModal,
   meta: { requiresAuth: true }
 }, {
   path: '*',
