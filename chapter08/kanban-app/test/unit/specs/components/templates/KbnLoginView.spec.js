c1f9ebaac49a4e0356b03ba3907693c94fb8d706
letitride <ichikawa.fumiya@gmail.com>
2020-04-21 20:10:20 +0900

    chapter10 login viewコンポーネントの実装

---
+++ chapter08/kanban-app/test/unit/specs/components/templates/KbnLoginView.spec.js
@@ -0,0 +1,11 @@
+import { mount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import KbnLoginView from '@/components/templates/KbnLoginView.vue'
+
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
+
+describe('KbnLoginView', () => {
+  
+})
