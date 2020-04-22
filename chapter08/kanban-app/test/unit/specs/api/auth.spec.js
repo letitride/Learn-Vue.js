acc6af2278d6b581fc6ede61c2fb204b932d0437
letitride <ichikawa.fumiya@gmail.com>
2020-04-22 22:19:09 +0900

    chapter10 ログインapi 失敗時のテスト

--- chapter08/kanban-app/test/unit/specs/api/auth.spec.js
+++ chapter08/kanban-app/test/unit/specs/api/auth.spec.js
@@ -32,5 +32,25 @@ describe('Auth APIモジュール', () => {
           .then(done)
       })
     })
+
+    describe('失敗', () => {
+      it('エラーメッセージを取得できること', done => {
+        const message = 'failed login'
+        const adapter = config => {
+          return new Promise((resolve, reject) => {
+            const err = new Error(message)
+            err.response = { data: { message }, status: 401 }
+            reject(err)
+          })
+        }
+
+        const auth = mockAuth(adapter)
+        auth.login({ address, password })
+          .catch(err => {
+            expect(err.message).to.equal(message)
+          })
+          .then(done)
+      })
+    })
   })
 })
