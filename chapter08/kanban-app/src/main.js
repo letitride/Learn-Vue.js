ea1f9a5b4bbb23b3f9e8a13e36c4ee78026f0e36
letitride <ichikawa.fumiya@gmail.com>
2020-04-23 12:04:19 +0900

    chapter10 グローバルなエラーハンドリング

--- chapter08/kanban-app/src/main.js
+++ chapter08/kanban-app/src/main.js
@@ -11,6 +11,12 @@ Vue.config.productionTip = process.env.NODE_ENV === 'production'
 
 Vue.component(ErrorBoundary.name, ErrorBoundary)
 
+Vue.config.errorHandler = (err, vm, info) => {
+  console.error('errorHandler err:', err)
+  console.error('errorHandler vm:', vm)
+  console.error('errorHandler info:', info)
+}
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
